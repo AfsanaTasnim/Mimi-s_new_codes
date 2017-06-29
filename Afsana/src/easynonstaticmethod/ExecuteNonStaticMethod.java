90cd09d1b98796ca55bb3c513418a4c05fc52de0
Afsana Tasnim <afsanatasnim780@yahoo.com>
2017-06-30 01:08:11 +0600

    This is another example of a non-static method. Here, an instance of the class is created.

---
+++ Afsana/src/easynonstaticmethod/ExecuteNonStaticMethod.java
@@ -0,0 +1,11 @@
+package easynonstaticmethod;
+
+public class ExecuteNonStaticMethod {
+
+	public static void main(String[] args) {
+		ShowNonStaticMethod c = new ShowNonStaticMethod();
+		c.display();
+
+	}
+
+}
---
+++ Afsana/src/easynonstaticmethod/ShowNonStaticMethod.java
@@ -0,0 +1,7 @@
+package easynonstaticmethod;
+
+public class ShowNonStaticMethod {
+	public void display() {
+		System.out.println("This is a non-static method.");
+	}
+}
